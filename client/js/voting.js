8cedbb7e8034637f9ab5d31760453fdf335a8765
TWcamel <j0930495159@gmail.com>
2020-04-18 20:24:50 +0800

    front-end-init

---
+++ client/js/voting.js
@@ -0,0 +1,16 @@
+let voteTo = null;
+
+function voteHandleClick(memberNumber) {
+    voteTo = memberNumber;
+    // console.log(memberNumber);
+    if (memberNumber == 1) {
+        $("#member-one-vote").css("max-width", "50%");
+        $("#member-one-vote").css("opacity", "100");
+    } else if (memberNumber == 2) {
+        $("#member-two-vote").css("max-width", "50%");
+        $("#member-two-vote").css("opacity", "100");
+    } else if (memberNumber == 3) {
+        $("#member-three-vote").css("max-width", "50%");
+        $("#member-three-vote").css("opacity", "100");
+    }
+}

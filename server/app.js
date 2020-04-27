e7ae61b45444108af7afcce51f8c60366ddc0b49
TWcamel <j0930495159@gmail.com>
2020-04-28 01:47:36 +0800

    add voteResult function in votin.js file

--- server/app.js
+++ server/app.js
@@ -14,3 +14,5 @@ require('./database/database.js');
 
 app.listen(config.serverPort,
     () => console.log(`Server is listening on port ${config.serverPort}.`));
+
+console.log(require('./libs/ccxpAuth.js').obtainServiceToken('107034549'));

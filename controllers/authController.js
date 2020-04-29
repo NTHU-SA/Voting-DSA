2728357d01c99e005ddb66c36e44034f556e87d9
楊宗翰 <feveraly@gmail.com>
2020-04-30 01:24:39 +0800

    ccxp OAuth signin another version

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,24 @@
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
 const ccxpAuth = require('../libs/ccxpAuth.js');
 const config = require('../config.js');
 
 module.exports = {
+
+    async authccxp(req, res) {
+        try {
+            const {account, passwd, captcha, ccxp_sessid} = req.body;
+            const code = await ccxpAuth.obtainCode(account, passwd, captcha, ccxp_sessid);
+            const tokenInfo = await ccxpAuth.verifyCode(code);
+            const userId = (await ccxpAuth.verifyAccessToken(
+                tokenInfo.access_token)).Userid;
+            const serviceToken = ccxpAuth.obtainServiceToken(userId);
+            res.cookie('service_token', serviceToken);
+            res.send({status: true});
+        } catch (e) {
+            res.status(401).send({status: false, error: 'Sign in to ccxp failed.'});
+        }
+    },
 
     async authccxpCallback(req, res) {
         try {
@@ -15,6 +31,13 @@ module.exports = {
         } catch (e) {
             res.status(401).send({status: false, error: e.message});
         }
+    },
+
+    async getccxpCaptchaImage(req, res) {
+        const response = await axios.get('https://oauth.ccxp.nthu.edu.tw/v1/vendor/securimage/3.6.7/securimage_show.php?fnstr=654321', {responseType: 'arraybuffer'});
+        res.cookie('ccxp_sessid', response.headers['set-cookie'][0].split('PHPSESSID=')[1].split(';')[0]);
+        res.contentType('image/png');
+        res.end(Buffer.from(response.data, 'binary'), 'binary');
     },
 
     async verifyServiceToken(req, res, next) {

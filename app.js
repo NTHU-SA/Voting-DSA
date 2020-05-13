0dc5aec1b4c2b258a1749954d515779d9dc96255
TWcamel <j0930495159@gmail.com>
2020-05-14 00:57:18 +0800

    rebuild front-end structure

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '.env.dev' });
+require('dotenv').config({path: '.env.dev'});
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./config.js');
@@ -6,14 +6,16 @@ const app = express();
 const Mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
 
-const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_NAME } = process.env;
+const {MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_NAME} = process.env;
 
-// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_NAME}?authSource=admin&w=1`;
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_NAME}`;
+// const url =
+// `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_NAME}?authSource=admin&w=1`;
+const url =
+    `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_NAME}`;
 Mongoose.connect(url, {
-    keepAlive: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  keepAlive: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 Mongoose.set('debug', true);
 
@@ -22,13 +24,13 @@ require('./models/votes');
 require('./models/activities');
 require('./models/options');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({type: 'application/json'}));
 app.use(fileUpload());
 
 app.use(require('./router'));
 app.use(express.static('./public'));
 
-app.listen(config.serverPort,
+app.listen(
+    config.serverPort,
     () => console.log(`Server is listening on port ${config.serverPort}.`));
-

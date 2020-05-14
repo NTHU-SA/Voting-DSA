700d89416d4a5d6fc058bbe3f3e64d30d3bd9c4f
TWcamel <j0930495159@gmail.com>
2020-05-14 21:28:52 +0800

    :wrench: front-end-template added

--- public/js/voting.js
+++ public/js/voting.js
@@ -1,174 +1,102 @@
-let voteOpt1 = null;
-let voteOpt2 = null;
-let voteOpt3 = null;
-let remark1 = null;
-let remark2 = null;
-let remark3 = null;
+const votes = {};
+const remarks = {
+  'remark1': '',
+  'remark2': '',
+  'remark3': ''
+};
+const optionIDs = new Array(Object.keys(remarks).length);
 
 function votesClick(
-    Opt, objYes, objYesUndo, objNo, objNoUndo, objWhatever, objWhateverUndo) {
+    Opt, objYes, yes, yesUndo, objNo, no, noUndo, objWhatever, whatever,
+    whateverUndo, bk1, bk2, bk3) {
+  objYes = $(objYes).attr('id');
+  yes = $(yes).attr('id');
+  yesUndo = $(yesUndo).attr('id');
+  objNo = $(objNo).attr('id');
+  no = $(no).attr('id');
+  noUndo = $(noUndo).attr('id');
+  objWhatever = $(objWhatever).attr('id');
+  whatever = $(whatever).attr('id');
+  whateverUndo = $(whateverUndo).attr('id');
+
+  bk1 = $(bk1).attr('id');
+  bk2 = $(bk2).attr('id');
+  bk3 = $(bk3).attr('id');
+  memberOption = bk1.split('bk1')[0];
+  idx = memberOption.split('member')[1]
+  remarkIdx = 'remark' + idx;
+  console.log(remarkIdx)
   if (Opt == 1) {
     $('#' + objYes).css('max-width', '50%');
     $('#' + objYes).css('opacity', '100%');
+    $('#' + yes).css('display', 'none');
+    $('#' + yesUndo).css('display', 'block');
+    $('#' + bk2).css('display', 'none');
+    $('#' + bk3).css('display', 'none');
+    votes[`${memberOption}`] = '我要投給他';
   }
+  else if (Opt == 2) {
+    $('#' + objNo).css('max-width', '50%');
+    $('#' + objNo).css('opacity', '100%');
+    $('#' + no).css('display', 'none');
+    $('#' + noUndo).css('display', 'block');
+    $('#' + bk1).css('display', 'none');
+    $('#' + bk3).css('display', 'none');
+    votes[`${memberOption}`] = '我不投給他';
+  }
+  else if (Opt == 3) {
+    $('#' + objWhatever).css('max-width', '50%');
+    $('#' + objWhatever).css('opacity', '100%');
+    $('#' + whatever).css('display', 'none');
+    $('#' + whateverUndo).css('display', 'block');
+    $('#' + bk1).css('display', 'none');
+    $('#' + bk2).css('display', 'none');
+    votes[`${memberOption}`] = '我沒有意見';
+  }
+  remarks[`${remarkIdx}`] = idx + '號候選人：' + votes[`${memberOption}`];
 }
 
-function voteOneClick(Opt) {
-  if (Opt == 1) {
-    voteOpt1 = '我要投給他';
-    $('#member-one-vote-yes').css('max-width', '50%');
-    $('#member-one-vote-yes').css('opacity', '100');
-    $('#oneYescancelNot').css('display', 'none');
-    $('#oneYescancel').css('display', 'block');
-    $('#member-one-2').css('display', 'none');
-    $('#member-one-3').css('display', 'none');
-  } else if (Opt == 2) {
-    voteOpt1 = '我不投給他';
-    $('#member-one-vote-no').css('max-width', '50%');
-    $('#member-one-vote-no').css('opacity', '100');
-    $('#oneNocancelNot').css('display', 'none');
-    $('#oneNocancel').css('display', 'block');
-    $('#member-one-1').css('display', 'none');
-    $('#member-one-3').css('display', 'none');
-  } else if (Opt == 3) {
-    voteOpt1 = Opt;
-    $('#member-one-vote-withoutOpinion').css('max-width', '50%');
-    $('#member-one-vote-withoutOpinion').css('opacity', '100');
-    $('#oneWOcancelNot').css('display', 'none');
-    $('#oneWOcancel').css('display', 'block');
-    $('#member-one-1').css('display', 'none');
-    $('#member-one-2').css('display', 'none');
-  }
-}
 
-function imgOneClick(Opt) {
-  if (Opt == 1) {
-    $('#member-one-vote-yes').css('max-width', '0%');
-    $('#member-one-vote-yes').css('opacity', '0');
-    $('#oneYescancelNot').css('display', 'block');
-    $('#oneYescancel').css('display', 'none');
-    $('#member-one-2').css('display', 'block');
-    $('#member-one-3').css('display', 'block');
-  } else if (Opt == 2) {
-    $('#member-one-vote-no').css('max-width', '0%');
-    $('#member-one-vote-no').css('opacity', '0');
-    $('#oneNocancelNot').css('display', 'block');
-    $('#oneNocancel').css('display', 'none');
-    $('#member-one-1').css('display', 'block');
-    $('#member-one-3').css('display', 'block');
-  } else if (Opt == 3) {
-    $('#member-one-vote-withoutOpinion').css('max-width', '0%');
-    $('#member-one-vote-withoutOpinion').css('opacity', '0');
-    $('#twoWOcancelNot').css('display', 'block');
-    $('#oneWOcancel').css('display', 'none');
-    $('#member-one-1').css('display', 'block');
-    $('#member-one-2').css('display', 'block');
-  }
-}
+function voteClickUndo(
+    Opt, objYes, yes, yesUndo, objNo, no, noUndo, objWhatever, whatever,
+    whateverUndo, bk1, bk2, bk3) {
+  objYes = $(objYes).attr('id');
+  yes = $(yes).attr('id');
+  yesUndo = $(yesUndo).attr('id');
 
-function voteTwoClick(Opt) {
-  if (Opt == 1) {
-    voteOpt2 = '我要投給他';
-    $('#member-two-vote-yes').css('max-width', '50%');
-    $('#member-two-vote-yes').css('opacity', '100');
-    $('#twoYescancelNot').css('display', 'none');
-    $('#twoYescancel').css('display', 'block');
-    $('#member-two-2').css('display', 'none');
-    $('#member-two-3').css('display', 'none');
-  } else if (Opt == 2) {
-    voteOpt2 = '我不投給他';
-    $('#member-two-vote-no').css('max-width', '50%');
-    $('#member-two-vote-no').css('opacity', '100');
-    $('#twoNocancelNot').css('display', 'none');
-    $('#twoNocancel').css('display', 'block');
-    $('#member-two-1').css('display', 'none');
-    $('#member-two-3').css('display', 'none');
-  } else if (Opt == 3) {
-    voteOpt2 = Opt;
-    $('#member-two-vote-withoutOpinion').css('max-width', '50%');
-    $('#member-two-vote-withoutOpinion').css('opacity', '100');
-    $('#twoWOcancelNot').css('display', 'none');
-    $('#twoWOcancel').css('display', 'block');
-    $('#member-two-1').css('display', 'none');
-    $('#member-two-2').css('display', 'none');
-  }
-}
+  objNo = $(objNo).attr('id');
+  no = $(no).attr('id');
+  noUndo = $(noUndo).attr('id');
 
-function imgTwoClick(Opt) {
-  if (Opt == 1) {
-    $('#member-two-vote-yes').css('max-width', '0%');
-    $('#member-two-vote-yes').css('opacity', '0');
-    $('#twoYescancelNot').css('display', 'block');
-    $('#twoYescancel').css('display', 'none');
-    $('#member-two-2').css('display', 'block');
-    $('#member-two-3').css('display', 'block');
-  } else if (Opt == 2) {
-    $('#member-two-vote-no').css('max-width', '0%');
-    $('#member-two-vote-no').css('opacity', '0');
-    $('#twoNocancelNot').css('display', 'block');
-    $('#twoNocancel').css('display', 'none');
-    $('#member-two-1').css('display', 'block');
-    $('#member-two-3').css('display', 'block');
-  } else if (Opt == 3) {
-    $('#member-two-vote-withoutOpinion').css('max-width', '0%');
-    $('#member-two-vote-withoutOpinion').css('opacity', '0');
-    $('#twoWOcancelNot').css('display', 'block');
-    $('#twoWOcancel').css('display', 'none');
-    $('#member-two-1').css('display', 'block');
-    $('#member-two-2').css('display', 'block');
-  }
-}
+  objWhatever = $(objWhatever).attr('id');
+  whatever = $(whatever).attr('id');
+  whateverUndo = $(whateverUndo).attr('id');
 
-function voteThreeClick(Opt) {
-  if (Opt == 1) {
-    voteOpt3 = '我要投給他';
-    $('#member-three-vote-yes').css('max-width', '50%');
-    $('#member-three-vote-yes').css('opacity', '100');
-    $('#threeYescancelNot').css('display', 'none');
-    $('#threeYescancel').css('display', 'block');
-    $('#member-three-2').css('display', 'none');
-    $('#member-three-3').css('display', 'none');
-  } else if (Opt == 2) {
-    voteOpt3 = '我不投給他';
-    $('#member-three-vote-no').css('max-width', '50%');
-    $('#member-three-vote-no').css('opacity', '100');
-    $('#threeNocancelNot').css('display', 'none');
-    $('#threeNocancel').css('display', 'block');
-    $('#member-three-1').css('display', 'none');
-    $('#member-three-3').css('display', 'none');
-  } else if (Opt == 3) {
-    voteOpt3 = '我沒有意見';
-    $('#member-three-vote-withoutOpinion').css('max-width', '50%');
-    $('#member-three-vote-withoutOpinion').css('opacity', '100');
-    $('#threeWOcancelNot').css('display', 'none');
-    $('#threeWOcancel').css('display', 'block');
-    $('#member-three-1').css('display', 'none');
-    $('#member-three-2').css('display', 'none');
-  }
-}
+  bk1 = $(bk1).attr('id');
+  bk2 = $(bk2).attr('id');
+  bk3 = $(bk3).attr('id');
 
-function imgThreeClick(Opt) {
   if (Opt == 1) {
-    $('#member-three-vote-yes').css('max-width', '0%');
-    $('#member-three-vote-yes').css('opacity', '0');
-    $('#threeYescancelNot').css('display', 'block');
-    $('#threeYescancel').css('display', 'none');
-    $('#member-three-2').css('display', 'block');
-    $('#member-three-3').css('display', 'block');
+    $('#' + objYes).css('max-width', '0%');
+    $('#' + objYes).css('opacity', '0%');
+    $('#' + yes).css('display', 'block');
+    $('#' + yesUndo).css('display', 'none');
+    $('#' + bk2).css('display', 'block');
+    $('#' + bk3).css('display', 'block');
   } else if (Opt == 2) {
-    $('#member-three-vote-no').css('max-width', '0%');
-    $('#member-three-vote-no').css('opacity', '0');
-    $('#threeNocancelNot').css('display', 'block');
-    $('#threeNocancel').css('display', 'none');
-    $('#member-three-1').css('display', 'block');
-    $('#member-three-3').css('display', 'block');
+    $('#' + objNo).css('max-width', '0%');
+    $('#' + objNo).css('opacity', '0%');
+    $('#' + no).css('display', 'block');
+    $('#' + noUndo).css('display', 'none');
+    $('#' + bk1).css('display', 'block');
+    $('#' + bk3).css('display', 'block');
   } else if (Opt == 3) {
-    $('#member-three-vote-withoutOpinion').css('max-width', '0%');
-    $('#member-three-vote-withoutOpinion').css('opacity', '0');
-    $('#threeWOcancelNot').css('display', 'block');
-    $('#threeWOcancel').css('display', 'none');
-    $('#member-three-1').css('display', 'block');
-    $('#member-three-2').css('display', 'block');
+    $('#' + objWhatever).css('max-width', '0%');
+    $('#' + objWhatever).css('opacity', '0%');
+    $('#' + whatever).css('display', 'block');
+    $('#' + whateverUndo).css('display', 'none');
+    $('#' + bk1).css('display', 'block');
+    $('#' + bk2).css('display', 'block');
   }
 }
 
@@ -184,17 +112,18 @@ async function checkVote() {
           .html('尚未選擇所有候選人哦！請您重新返回投票頁面進行投票');
       $('.btn-modalInfoSecondary').css('display', 'none');
     } else {
-      $('.modalInfo')
-          .html(
-              '您的所選擇的候選人依序爲<br>' +
-              '一號候選人：' + voteOpt1 + '<br>' +
-              '二號候選人：' + voteOpt2 + '<br>' +
-              '三號候選人：' + voteOpt3);
+      const votesMarkup = '您的所選擇的候選人依序爲<br>' +
+          '{{each(i,val) votes}}' +
+          '<p>' +
+          '${i}：' +
+          '號候選人：' +
+          '${val}' +
+          '</p>' +
+          '{{/each}}';
+      $.template('votesTemplate', votesMarkup);
+      $.tmpl('votesTemplate', votes).appendTo('.modalInfo');
       $('.btn-modalInfoPrimary').css('display', 'inline');
       $('.btn-modalInfoSecondary').css('display', 'inline');
-      remark1 = `一號候選人：${voteOpt1}`;
-      remark2 = `二號候選人：${voteOpt2}`;
-      remark3 = `三號候選人：${voteOpt3}`;
     }
   } catch (e) {
     console.log(e.response.data);
@@ -250,6 +179,9 @@ async function sendUserResult() {
           'sort': 0,
         },
         {});
+    // optionIDs.forEach(ele, index => {
+    //   ele = resOption.data.data[index]._id;
+    // });
     optionID1 = resOption.data.data[0]._id;
     optionID2 = resOption.data.data[1]._id;
     optionID3 = resOption.data.data[2]._id;
@@ -260,16 +192,16 @@ async function sendUserResult() {
           'choose_all': [
             {
               'option_id': optionID1,
-              'remark': remark1,
+              'remark': remarks.remark1,
             },
             {
               'option_id': optionID2,
-              'remark': remark2,
+              'remark': remarks.remark2,
             },
             {
               'option_id': optionID3,
-              'remark': remark3,
-            }
+              'remark': remarks.remark3,
+            },
           ],
         },
         {

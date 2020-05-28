5e0c8728a4302c3ae17c9e2dbb9f596e16ddfda7
TWcamel <j0930495159@gmail.com>
2020-05-29 04:35:57 +0800

    fix btn issue

--- public/js/voting.js
+++ public/js/voting.js
@@ -1,7 +1,9 @@
 $(document).ready(() => {
     $('#chooseTypeModal').modal('show');
     getAvailableActivities();
-    $('.btn-chooseType').click(() => {
+    $('.btn-result').hide();
+    $('.btn-voteIndo').click(()=>{
+        $('.btn-result').show();
     });
 });
 
@@ -49,7 +51,7 @@ const chooseOneClick = (
     for (i = 4; i < 4 + 2 + 1; i++) paramsList[Object.keys(paramsList)[i]] = 'member' + (i - 3) + 'bk' + 1;
 
     if (Opt === 1) {
-        attr2ID(paramsList.objYes).css('max-width', '50%');
+        attr2ID(paramsList.objYes).css('max-width', '15%');
         attr2ID(paramsList.objYes).css('opacity', '100%');
         attr2ID(paramsList.yes).css('display', 'none');
         attr2ID(paramsList.yesUndo).css('display', 'block');
@@ -57,12 +59,15 @@ const chooseOneClick = (
         if (memberOption === 'member1') {
             attr2ID(paramsList.memberbk2).hide();
             attr2ID(paramsList.memberbk3).hide();
+            $('.img-click-icon').css('max-width', '4%');
         } else if (memberOption === 'member2') {
             attr2ID(paramsList.memberbk1).hide();
             attr2ID(paramsList.memberbk3).hide();
+            $('.img-click-icon').css('max-width', '4%');
         } else if (memberOption === 'member3') {
             attr2ID(paramsList.memberbk1).hide();
             attr2ID(paramsList.memberbk2).hide();
+            $('.img-click-icon').css('max-width', '4%');
         }
         votes[`${memberOption}`] = idx;
     } else if (Opt === 0) {
@@ -73,12 +78,15 @@ const chooseOneClick = (
         if (memberOption === 'member1') {
             attr2ID(paramsList.memberbk2).show();
             attr2ID(paramsList.memberbk3).show();
+            $('.img-click-icon').css('max-width', '12%');
         } else if (memberOption === 'member2') {
             attr2ID(paramsList.memberbk1).show();
             attr2ID(paramsList.memberbk3).show();
+            $('.img-click-icon').css('max-width', '12%');
         } else if (memberOption === 'member3') {
             attr2ID(paramsList.memberbk1).show();
             attr2ID(paramsList.memberbk2).show();
+            $('.img-click-icon').css('max-width', '12%');
         }
         votes = {};
     }
@@ -113,28 +121,31 @@ const chooseAllClick = (
     remarkIdx = 'remark' + memberOption;
 
     if (Opt === 1) {
-        attr2ID(paramsList.objYes).css('max-width', '50%');
+        attr2ID(paramsList.objYes).css('max-width', '15%');
         attr2ID(paramsList.objYes).css('opacity', '100%');
         attr2ID(paramsList.yes).css('display', 'none');
         attr2ID(paramsList.yesUndo).css('display', 'block');
         attr2ID(paramsList.bk2).css('display', 'none');
         attr2ID(paramsList.bk3).css('display', 'none');
+        $('.img-click-icon').css('max-width', '4%');
         votes[`${memberOption}`] = '我要投給他';
     } else if (Opt === 2) {
-        attr2ID(paramsList.objNo).css('max-width', '50%');
+        attr2ID(paramsList.objNo).css('max-width', '15%');
         attr2ID(paramsList.objNo).css('opacity', '100%');
         attr2ID(paramsList.no).css('display', 'none');
         attr2ID(paramsList.noUndo).css('display', 'block');
         attr2ID(paramsList.bk1).css('display', 'none');
         attr2ID(paramsList.bk3).css('display', 'none');
+        $('.img-click-icon').css('max-width', '4%');
         votes[`${memberOption}`] = '我不投給他';
     } else if (Opt === 3) {
-        attr2ID(paramsList.objWhatever).css('max-width', '50%');
+        attr2ID(paramsList.objWhatever).css('max-width', '15%');
         attr2ID(paramsList.objWhatever).css('opacity', '100%');
         attr2ID(paramsList.whatever).css('display', 'none');
         attr2ID(paramsList.whateverUndo).css('display', 'block');
         attr2ID(paramsList.bk1).css('display', 'none');
         attr2ID(paramsList.bk2).css('display', 'none');
+        $('.img-click-icon').css('max-width', '4%');
         votes[`${memberOption}`] = '我沒有意見';
     } else if (Opt == -2) {
         attr2ID(paramsList.objYes).css('max-width', '0%');
@@ -143,6 +154,7 @@ const chooseAllClick = (
         attr2ID(paramsList.yesUndo).css('display', 'none');
         attr2ID(paramsList.bk2).css('display', 'block');
         attr2ID(paramsList.bk3).css('display', 'block');
+        $('.img-click-icon').css('max-width', '12%');
         votes[`${memberOption}`] = undefined;
     } else if (Opt == -1) {
         attr2ID(paramsList.objNo).css('max-width', '0%');
@@ -151,6 +163,7 @@ const chooseAllClick = (
         attr2ID(paramsList.noUndo).css('display', 'none');
         attr2ID(paramsList.bk1).css('display', 'block');
         attr2ID(paramsList.bk3).css('display', 'block');
+        $('.img-click-icon').css('max-width', '12%');
         votes[`${memberOption}`] = undefined;
     } else if (Opt == 0) {
         attr2ID(paramsList.objWhatever).css('max-width', '0%');
@@ -159,6 +172,7 @@ const chooseAllClick = (
         attr2ID(paramsList.whateverUndo).css('display', 'none');
         attr2ID(paramsList.bk1).css('display', 'block');
         attr2ID(paramsList.bk2).css('display', 'block');
+        $('.img-click-icon').css('max-width', '12%');
         votes[`${memberOption}`] = undefined;
     }
     remarks[`${remarkIdx}`] = `${candidateName} :` + votes[`${memberOption}`];
@@ -208,10 +222,10 @@ async function checkVote() {
             }
         }
     } catch (e) {
-        console.log(e);
-        $('#modalTokan-title').innerHTML = "失敗";
+        document.getElementById('modalTokan-title').innerHTML='失敗';
         $('.modalToken').html(`<p>出錯了&#128563 ${e}</p>`);
         $('#modalToken').modal('show');
+        console.log(e);
     }
 }
 
@@ -247,7 +261,7 @@ async function sendUserResult() {
                     'remark': votes[`member${i + 1}`],
                 });
             }
-            console.log(voteContent)
+            console.log(voteContent);
             resp = await axios.post(
                 '/votes/addVote', {
                     'activity_id': activityID,
@@ -258,6 +272,7 @@ async function sendUserResult() {
                         Authorization: `Bearer ${jwtToken}`,
                     },
                 });
+            $('#modalTokan-title').innerHTML = '投票成功！';
             $('.modalToken').html(`投票成功！<br>請保存好您的存根權杖，以供驗票使用：<br>${resp.data.token}`);
             $('#modalToken').modal('show');
         } else if (chooseType.chooseAll === 0) {
@@ -276,7 +291,7 @@ async function sendUserResult() {
                 });
         }
     } catch (e) {
-        $('#modalTokan-title').innerHTML = "失敗";
+        document.getElementById('modalTokan-title').innerHTML='投票成功';
         $('.modalToken').html(`<p>出錯了&#128563 ${e}</p>`);
         $('#modalToken').modal('show');
         console.log(e.response);

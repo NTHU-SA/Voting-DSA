2c0207315fe9613676f13611548916fc79118289
oxygen-TW <oxygen5307@gmail.com>
2022-03-09 00:31:40 +0800

    完成新增管理員介面

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,6 +3,7 @@ var adminList = [];
 async function setAdmin(toAdmin) {
     const remark = toAdmin ? "admin" : "";
     const stuId = document.getElementById("stuId").value;
+    const msg = toAdmin ? `成功新增 ${stuId} 為管理員`:`成功將 ${stuId} 從管理員刪除`;
 
     try {
         await axios.post('/users/modifyUser', {
@@ -11,7 +12,8 @@ async function setAdmin(toAdmin) {
             "byStuId": true //指定以 student_id 為 key update，ref: pullrequest #4
         }).then((resp) => {
             if(resp.data["success"]);
-                alert(`成功新增 ${stuId} 為管理員`);
+                alert(msg);
+                document.getElementById("stuId").value = ""; //清空Input
         });
     } catch (error) {
         alert("ERROR" + error);

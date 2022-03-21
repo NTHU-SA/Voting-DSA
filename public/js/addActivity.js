01849f663f8b8e756a8d4134f6abfc82912efd48
dinoslow <73374146+dinoslow@users.noreply.github.com>
2022-03-21 23:44:10 +0800

    Fix submit bug

--- public/js/addActivity.js
+++ public/js/addActivity.js
@@ -1,109 +1,88 @@
 const options = [];
 const viceExp = {};
+// viceExp = {
+//      1:{vice1: 0, vice2: 0},
+//      2:{vice1: 0, vice2: 0},
+//      ...
+// }
 async function addAct() {
-    const NAME = document.getElementById("name").value;
-    const RULE = document.getElementById("rule").value;
-    const OPEN_FROM = document.getElementById("open_from").value.replaceAll('/', '-').replace('T', ' ')+":00";
-    const OPEN_TO = document.getElementById("open_to").value.replaceAll('/', '-').replace('T', ' ')+":00";
-    console.log("OPEN FROM: " + OPEN_FROM);
-    console.log("OPEN_TO: " + OPEN_TO);
-    console.log(jwtToken);
-    try {
-        await axios.post(
-            '/activities/addActivity', {
-                'name': NAME,
-                'type': 'candidate',
-                'rule': RULE,
-                'open_from': OPEN_FROM,
-                'open_to': OPEN_TO,
-            }, {
-                headers: {
-                    Authorization: `Bearer ${jwtToken}`,
-                },
-            }).then(res => {
-                console.log(res.data);
-                const { name, _id } = res.data;
-                addOption(name);
-            }).catch(err => {
-                console.log(err);
-            });
-    } catch (e) {
-        console.log(e.response.data);
+    if(confirm('確定送出嗎?')) {
+        const NAME = document.getElementById("name").value;
+        const RULE = document.getElementById("rule").value;
+        const OPEN_FROM = document.getElementById("open_from").value.replaceAll('/', '-').replace('T', ' ');
+        const OPEN_TO = document.getElementById("open_to").value.replaceAll('/', '-').replace('T', ' ');
+        // console.log(jwtToken);
+
+        if(NAME == "" || NAME == undefined) {
+            alert("請將活動名稱填寫完整");
+            return;
+        } else if(OPEN_FROM == "" || OPEN_TO == "") {
+            alert("請將開始及結束時間填寫完整");
+            return;
+        } else if(OPEN_FROM > OPEN_TO) {
+            alert("結束時間小於開始時間，請填入正確的開始及結束時間");
+            return;
+        }
+
+        try {
+            const resActivity = await axios.post(
+                '/activities/getActivities', {
+                    'filter': { name: NAME },
+                }, {
+                    headers: {
+                        Authentication:
+                        `Bearer ${document.cookie.split('service_token=')[1]} `,
+                    },
+                });
+            // console.log(resActivity);
+            if(resActivity.data.total > 0) {
+                console.log("exist");
+                addOption(resActivity.data.data[0]._id);
+            } else {
+                console.log("not exist");
+                await axios.post(
+                    '/activities/addActivity', {
+                        'name': NAME,
+                        'type': 'candidate',
+                        'rule': RULE,
+                        'open_from': OPEN_FROM + ":00",
+                        'open_to': OPEN_TO + ":00",
+                    }, {
+                        headers: {
+                            Authorization: `Bearer ${jwtToken}`,
+                        },
+                    }).then(res => {
+                        console.log(res.data);
+                        const { _id } = res.data;
+                        addOption(_id);
+                    }).catch(err => {
+                        console.log(err);
+                    });
+            }
+        } catch (e) {
+            console.log(e.response.data);
+        }
+    } else {
+        return;
     }
 }
 
-async function addOption(name) {
-    candidates = [];
-    vices = [];
-    for (i = 1; i <= options.length; i++) {
-        let candidateName = document.getElementById(`option-${i}-name`).value;
-        let candidateDepartment = document.getElementById(`option-${i}-department`).value;
-        let candidateCollege = document.getElementById(`option-${i}-college`).value;
-        let candidateAvatarUrl = document.getElementById(`option-${order}-avatar_url`).value;
-        let candidatePersonalExperiences = [];
-        for(j = 1; j <= options[i-1].personal_experiences; j++) {
-            let ex = document.getElementById(`option-${i}-exp-${j}`).value;
-            candidatePersonalExperiences.push(ex);
-        }
-        let candidatePoliticalOptions = [];
-        for(k = 1; k <= options[i-1].political_options; k++) {
-            let op = document.getElementById(`option-${i}-political-${k}`).value;
-            candidatePoliticalOptions.push(`${k}. `+ op);
-        }
-        if(options[i-1].vice_options) {
-            vice = [];
-            for(l = 1; l <= 2; l++) {
-                let viceName = document.getElementById(`option-vice-${i}-${l}-name`).value;
-                let viceDepartment = document.getElementById(`option-vice-${i}-${l}-department`).value;
-                let viceCollege = document.getElementById(`option-vice-${i}-${l}-college`).value;
-                let viceAvatarUrl = document.getElementById(`option-vice-${i}-${l}-avatar_url`).value;
-                let vicePersonalExperiences = [];
-                if(l == 1) {
-                    for(m = 1; m <= viceExp[i].vice1; m++) {
-                        let vex = document.getElementById(`option-vice-${i}-${l}-exp-${m}`).value;
-                        vicePersonalExperiences.push(vex);
-                    }
-                } else {
-                    for(m = 1; m <= viceExp[i].vice2; m++) {
-                        let vex = document.getElementById(`option-vice-${i}-${l}-exp-${m}`).value;
-                        vicePersonalExperiences.push(vex);
-                    }
-                }
-                vice.push({
-                    "name": viceName,
-                    "department": viceDepartment,
-                    "college": viceCollege,
-                    "avatar_url": viceAvatarUrl,
-                    "personal_experiences": vicePersonalExperiences,
-                });
-            };
-            vices[`${i}`] = vice;
-        }
-        candidates.push({
-            "name": candidateName,
-            "department": candidateDepartment,
-            "college": candidateCollege,
-            "avatar_url": candidateAvatarUrl,
-            "personal_experiences": candidatePersonalExperiences,
-            "political_opinions": candidatePoliticalOptions,
-        })
-    }
+async function addOption(activityID) {
+    const candidates = [];
+    const vices = [];
+    // TODO: pass valid
+    const valid = await getFieldValue(candidates, vices);
     console.log(candidates);
-    console.log(vices);
+    console.log(valid);
     try {
-        const resActivity = await axios.post(
-            '/activities/getActivities', {
-                'filter': { name: name },
-            }, {
-                headers: {
-                    Authentication:
-                    `Bearer ${document.cookie.split('service_token=')[1]} `,
-                },
-            });
-        activityID = resActivity.data.data[0]._id;
+        if(options.length == 0) {
+            alert("請填寫候選人");
+            return;
+        } else if(!valid) {
+            alert("請將表格填寫完整");
+            return;
+        }
         for (i = 1; i <= options.length; i++) {
-            console.log(vices[i]);
-            console.log(options[i-1].vice_options);
             if(options[i-1].vice_options > 0) {
                 axios.post('/options/addOption', {
                     'activity_id': activityID,
@@ -130,7 +109,8 @@ async function addOption(name) {
                 });
             }
         }
-        console.log("Add option success");
+        alert("送出成功!將回到上一頁");
+        // window.location = "/activity.html";
     } catch (e) {
         console.log(e);
     }
@@ -160,33 +140,169 @@ async function addImg(src) {
     }
 }
 
+async function getFieldValue(candidates, vices) {
+    let valid = true;
+    for (i = 1; i <= options.length; i++) {
+        let candidateName = document.getElementById(`option-${i}-name`).value;
+        let candidateDepartment = document.getElementById(`option-${i}-department`).value;
+        let candidateCollege = document.getElementById(`option-${i}-college`).value;
+        let candidateAvatarUrl = document.getElementById(`option-${i}-avatar_url`).value;
+        // check valid
+        if(candidateName == "" || candidateDepartment == "" || candidateCollege == "" || candidateAvatarUrl == "") valid = false;
+
+        let candidatePersonalExperiences = [];
+        for(j = 1; j <= options[i-1].personal_experiences; j++) {
+            let ex = document.getElementById(`option-exp-${i}-${j}`).value;
+            // check valid
+            if(ex == "") {
+                valid = false;
+                return;
+            }
+            candidatePersonalExperiences.push(ex);
+        }
+        let candidatePoliticalOptions = [];
+        for(k = 1; k <= options[i-1].political_options; k++) {
+            let op = document.getElementById(`option-political-${i}-${k}`).value;
+            // check valid
+            if(op == "") {
+                valid = false;
+                return;
+            }
+            candidatePoliticalOptions.push(`${k}. `+ op);
+        }
+        if(options[i-1].vice_options > 0) {
+            vice = [];
+            for(l = 1; l <= 2; l++) {
+                let viceName = document.getElementById(`option-vice-${i}-${l}-name`).value;
+                let viceDepartment = document.getElementById(`option-vice-${i}-${l}-department`).value;
+                let viceCollege = document.getElementById(`option-vice-${i}-${l}-college`).value;
+                let viceAvatarUrl = document.getElementById(`option-vice-${i}-${l}-avatar_url`).value;
+                // check valid
+                if(viceName == "" || viceDepartment == "" || viceCollege == "" || viceAvatarUrl == "") valid = false;
+                let vicePersonalExperiences = [];
+                if(l == 1) {
+                    for(m = 1; m <= viceExp[i].vice1; m++) {
+                        let vex = document.getElementById(`option-vice-exp-${i}-${l}-${m}`).value;
+                        // check valid
+                        if(vex == "") {
+                            valid = false;
+                            return;
+                        }
+                        vicePersonalExperiences.push(vex);
+                    }
+                } else {
+                    for(m = 1; m <= viceExp[i].vice2; m++) {
+                        let vex = document.getElementById(`option-vice-exp-${i}-${l}-${m}`).value;
+                        // check valid
+                        if(vex == "") {
+                            valid = false;
+                            return;
+                        }
+                        vicePersonalExperiences.push(vex);
+                    }
+                }
+                if(!valid) return;
+                vice.push({
+                    "name": viceName,
+                    "department": viceDepartment,
+                    "college": viceCollege,
+                    "avatar_url": viceAvatarUrl,
+                    "personal_experiences": vicePersonalExperiences,
+                });
+            };
+            // not valid
+            if(!valid) return;
+            vices[`${i}`] = vice;
+        }
+        // check valid
+        if(!valid) return;
+        candidates.push({
+            "name": candidateName,
+            "department": candidateDepartment,
+            "college": candidateCollege,
+            "avatar_url": candidateAvatarUrl,
+            "personal_experiences": candidatePersonalExperiences,
+            "political_opinions": candidatePoliticalOptions,
+        })
+        // valid
+        return true;
+    }
+}
+
+// <div class="exp-{candidate_index}-{exp_index}">
+//      <label for="option-exp-{candidate_index}-{exp_index}"></label>
+//      <input id="option-exp-{candidate_index}-{exp_index}"/>
+// </div>
 function addPersonalExpField(order) {
+    // check last personal exp valid
+    const  current = options[order - 1].personal_experiences;
+    if(current > 0) {
+        const valid = document.getElementById(`option-exp-${order}-${current}`).value;
+        if(valid == "") {
+            alert("請先填完上一個候選人經歷");
+            return;
+        }
+    }
+    // add new personal exp field
     optionsNode = document.getElementById(`option-${order}-exp`);
     options[order - 1].personal_experiences += 1;
     let newOrder = options[order - 1].personal_experiences;
     html = document.createElement('div');
-    html.className = `exp-${order}-${newOrder}`; // class for delete
+    html.setAttribute("id", `exp-${order}-${newOrder}`);
     html.innerHTML = `
-        <label for='option-${order}-exp-${newOrder}'>經歷${newOrder}：</label>
-        <input id='option-${order}-exp-${newOrder}' class="form-control" required="required"/>
+        <label for='option-exp-${order}-${newOrder}'>經歷${newOrder}：</label>
+        <input id='option-exp-${order}-${newOrder}' class="form-control" required="required"/>
     `;
     optionsNode.appendChild(html);
 }
 
+// <div class="political-{candidate_index}-{pol_index}">
+//      <label for="option-political-{candidate_index}-{pol_index}"></label>
+//      <input id="option-political-{candidate_index}-{pol_index}"/>
+// </div>
 function addPoliticalField(order) {
+    // check last political opinion valid
+    const current = options[order - 1].political_options;
+    if(current > 0) {
+        const valid = document.getElementById(`option-political-${order}-${current}`).value;
+        if(valid == "") {
+            alert("請先填完上一個候選人政見");
+            return;
+        }
+    }
+    // add new political field
     optionsNode = document.getElementById(`option-${order}-political`);
     options[order - 1].political_options += 1;
     let newOrder = options[order - 1].political_options;
     html = document.createElement('div');
-    html.className = `political-${order}-${newOrder}`; // class for delete
+    html.setAttribute("id", `political-${order}-${newOrder}`);
     html.innerHTML = `
-        <label for='option-${order}-political-${newOrder}'>政見${newOrder}：</label>
-        <input id='option-${order}-political-${newOrder}' class="form-control" required="required"/>
+        <label for='option-political-${order}-${newOrder}'>政見${newOrder}：</label>
+        <input id='option-political-${order}-${newOrder}' class="form-control" required="required"/>
     `;
     optionsNode.appendChild(html);
 }
 
+// <div class="exp-vice-{candidate_index}-{candidate's_vice_index}-{vice_exp_index}">
+//      <label for="option-vice-exp-{candidate_index}-{candidate's_vice_index}-{vice_exp_index}"></label>
+//      <input id="option-vice-exp-{candidate_index}-{candidate's_vice_index}-{vice_exp_index}"/>
+// </div>
 function addVicePersonalExpField(order, newOrder) {
+    // check last vice personal exp valid
+    let current;
+    if(newOrder == 1) {
+        current = viceExp[order].vice1;
+    } else {
+        current = viceExp[order].vice2;
+    }
+    if(current > 0) {
+        const valid = document.getElementById(`option-vice-exp-${order}-${newOrder}-${current}`).value;
+        if(valid == "") {
+            alert("請先填完上一個副候選人經歷");
+            return;
+        }
+    }
+    // add new vice personal exp field
     let idx;
     optionsNode = document.getElementById(`option-vice-${order}-${newOrder}-exp`);
     if(newOrder == 1) {
@@ -197,24 +313,30 @@ function addVicePersonalExpField(order, newOrder) {
         idx = viceExp[order].vice2;
     }
     html = document.createElement('div');
-    html.className = `exp-vice-${order}-${newOrder}-${idx}`; // class for delete
+    html.setAttribute("id", `exp-vice-${order}-${newOrder}-${idx}`); // class for delete
     html.innerHTML = `
-        <label for='option-vice-${order}-${newOrder}-exp-${idx}'>經歷${idx}：</label>
-        <input id='option-vice-${order}-${newOrder}-exp-${idx}' class="form-control" required="required"/>
+        <label for='option-vice-exp-${order}-${newOrder}-${idx}'>經歷${idx}：</label>
+        <input id='option-vice-exp-${order}-${newOrder}-${idx}' class="form-control" required="required"/>
     `;
     optionsNode.appendChild(html);
 }
 
+// <div class="vice-{candidate_index}-{vice_index}">
+//      ...
+//      <label for="option-vice-{candidate_idx}-{1 or 2}-{vice_candidate's_label}"></label>
+//      <input id="option-vice-{candidate_idx}-{1 or 2}-{vice_candidate's_label}"/>
+//      ...
+// </div>
 function addViceField(order) {
     optionsNode = document.getElementById(`option-${order}-vice`);
-    options[order - 1].vice_options += 1;
+    options[order - 1].vice_options += 2;
     let newOrder = options[order - 1].vice_options;
     viceExp[order] = {
         vice1: 0,
         vice2: 0
     }
     html = document.createElement('div');
-    html.className = `vice-${order}-${newOrder}`;
+    html.setAttribute("id", `vice-${order}-${newOrder}`);
     html.innerHTML = `
         <br>
         <h4>${order}號副候選人之一：</h4>
@@ -246,6 +368,7 @@ function addViceField(order) {
         <div class="center">
         <button type="button" class="btn btn-primary btn-sm" onclick="addVicePersonalExpField(${order}, 2)">新增副候選人經歷</button>
         </div>
+        <button type="button" class="btn btn-info" onclick="closeVicePersonalExpField(${order}, ${newOrder})">刪除副候選人</button>
         <br>
     `;
     optionsNode.appendChild(html);
@@ -253,11 +376,43 @@ function addViceField(order) {
     hideNode.style.display = 'none';
 }
 
+function closeVicePersonalExpField(order, newOrder) {
+    // remove candidate's vice
+    options[order - 1].vice_options -= 2;
+    // remove vice's exp
+    viceExp[`${order}`].vice1 = 0;
+    viceExp[`${order}`].vice2 = 0;
+
+    const deleteNode = document.getElementById(`vice-${order}-${newOrder}`);
+    deleteNode.remove();
+
+    const showNode = document.getElementById(`option-${order}-vice-btn`);
+    showNode.style.display = 'block';
+}
+
+// <div class="candidateField-{candidate_index}">
+//      ...
+//      <label for="option-{candidate_index}-{candidate's_label}"></label>
+//      <input id="option-{candidate_index}-{candidate's_label}"/>
+//      ...
+// </div>
 function addOptionField() {
+    // check if last candidate field valid
+    if(options.length > 0) {
+        let name_valid = document.getElementById(`option-${options.length}-name`).value;
+        let department_valid = document.getElementById(`option-${options.length}-department`).value;
+        let college_valid = document.getElementById(`option-${options.length}-college`).value;
+        let avatar_url_valid = document.getElementById(`option-${options.length}-avatar_url`).value;
+        if(name_valid == "" || department_valid == "" || college_valid == "" || avatar_url_valid == "") {
+            alert('請先填完上一位候選人');
+            return;
+        }
+    }
+    // add new candidate field
     optionsNode = document.getElementById('options');
     order = options.length + 1;
     html = document.createElement('div');
-    html.className = `candidateField-${order}`;
+    html.setAttribute("id", `candidateField-${order}`);
     html.innerHTML = `
         <br>
         <h4>${order}號候選人：</h4>
